e3945b09192e35dce5577cc30027f95241c6ab07
Jeremy Apthorp <jeremya@chromium.org>
2019-05-30 14:45:24 -0700

    refactor: simplify content script injection

--- atom/common/api/api_messages.h
+++ atom/common/api/api_messages.h
@@ -29,9 +29,6 @@ IPC_MESSAGE_ROUTED0(AtomAutofillFrameHostMsg_HidePopup)
 IPC_MESSAGE_ROUTED1(AtomAutofillFrameMsg_AcceptSuggestion,
                     base::string16 /* suggestion */)
 
-// Update renderer process preferences.
-IPC_MESSAGE_CONTROL1(AtomMsg_UpdatePreferences, base::ListValue)
-
 // Sent by renderer to set the temporary zoom level.
 IPC_SYNC_MESSAGE_ROUTED1_1(AtomFrameHostMsg_SetTemporaryZoomLevel,
                            double /* zoom level */,
